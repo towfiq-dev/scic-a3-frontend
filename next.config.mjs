ca90ae4272f006c5b8f098b15c368ff8b456ec7a
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-07-21 23:30:43 +0600

    next config.mjs update

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,64 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '**',
+//         port: '',
+//         pathname: '**',
+//         search: '',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'cdn.pixabay.com',
+//         port: '',
+//         pathname: '**',
+//         search: '',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'images.unsplash.com',
         port: '',
-        pathname: '**',
-        search: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.pixabay.com',
         port: '',
-        pathname: '**',
-        search: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'lh3.googleusercontent.com', // Google login photo-র জন্য
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
         port: '',
         pathname: '/**',
       },
